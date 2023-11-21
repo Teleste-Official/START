b12d8da5d2f2eb90cffe198ec1552f0862ae7b0e
Metso <risenoutcast@gmail.com>
2023-11-21 10:48:17 +0200

    Added train models

---
+++ Models/Train.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrainApplication.Models
+{
+    public class Train
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float MaxSpeed { get; set; }
+        public int Icon {  get; set; }
+
+        public Train() { }
+
+        public Train(string name, string description, float maxSpeed)
+        {
+            Name = name;
+            Description = description;
+            MaxSpeed = maxSpeed;
+            Icon = 0;
+        }
+    }
+}
