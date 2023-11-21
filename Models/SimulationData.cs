6168f1686aef65a79ab0ffc403bdaa49af72ef05
Metso <risenoutcast@gmail.com>
2023-11-21 12:24:54 +0200

    Added simulation models

---
+++ Models/SimulationData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrainApplication.Models
+{
+    public class SimulationData
+    {
+        string Name { get; set; } // Datetime?
+
+        List<TickData> TickData { get; set; }
+
+        public SimulationData() { }
+
+        public SimulationData(string name, List<TickData> tickData)
+        {
+            Name = name;
+            TickData = tickData;
+        }
+    }
+}
+
