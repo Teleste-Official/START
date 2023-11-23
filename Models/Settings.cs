f7f95fc2024399d2f85a4c0e70297465dc713eeb
Metso <risenoutcast@gmail.com>
2023-11-23 23:49:02 +0200

    Updated Settings

---
+++ Models/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrainApplication.Models
+{
+    internal class Settings
+    {
+        public string Longitude {  get; set; }
+        public string Latitude { get; set; }
+        public List<string> RouteDirectories { get; set; }
+        public List<string> TrainDirectories { get; set; }
+        public string VersionNumber { get; set; }
+
+        public Settings() { }
+
+        public Settings(string longitude, string latitude, List<string> routeDirectories, List<string> trainDirectories)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+            RouteDirectories = routeDirectories;
+            TrainDirectories = trainDirectories;
+        }
+    }
+}
